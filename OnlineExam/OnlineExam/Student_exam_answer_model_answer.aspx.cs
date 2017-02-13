e76126ccc320a5eef39f70f44e979f8b3df81d2f
Phantom <Phantom@DESKTOP-JHICASB>
2017-02-13 15:41:18 -0800

    Student_exam_answer_model_answer added

---
+++ OnlineExam/OnlineExam/Student_exam_answer_model_answer.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineExam
+{
+    public partial class Student_exam_answer_model_answer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ddlStudent.DataSource = Display.DisplayStudentByID();
+                ddlStudent.DataValueField = "St-ID";
+                ddlStudent.DataTextField = "St-ID";
+                ddlStudent.DataBind();
+                ddlExam.DataSource = Reports.DisplayExamByID();
+                ddlExam.DataValueField = "Exam-ID";
+                ddlExam.DataTextField = "Exam-ID";
+                ddlExam.DataBind();
+
+            }
+
+        }
+
+        protected void ddlStudent_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvViewQues_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ddlExam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnshow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                gvViewQues.DataSource = Display.Student_exam_answer_model_answer(int.Parse(ddlExam.SelectedValue), int.Parse(ddlStudent.SelectedValue));
+                gvViewQues.DataBind();
+            }
+            catch
+            {
+                lblresult.Text = "Error In Exam Or Student";
+            }
+        }
+    }
+}
